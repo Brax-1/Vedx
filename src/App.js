3c8f5794c00cb12f7a35eef9c139ba48d6d7dbfa
Brax-1 <ay701548@gmail.com>
2022-01-30 10:46:32 +0530

    vedx_assignment V1.2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
       mainarticle: [],
       article: [],
       sync: true,
-      Heading:"All Orders",
+      Heading: "All Orders",
     }
   }
   async componentDidMount() {
@@ -26,75 +26,76 @@ export default class App extends Component {
     if (value === "order_id") {
       const sortedData = data.state.article.sort((x, y) => x.order_id < y.order_id ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Order Id"});
+      data.setState({ Heading: "Sorted By Order Id" });
     }
     else if (value === "customer") {
       const sortedData = data.state.article.sort((x, y) => x.customer < y.customer ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Customer"});
+      data.setState({ Heading: "Sorted By Customer" });
     }
     else if (value === "country") {
       const sortedData = data.state.article.sort((x, y) => x.country < y.country ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Country"});
+      data.setState({ Heading: "Sorted By Country" });
     }
     else if (value === "address") {
       const sortedData = data.state.article.sort((x, y) => x.address < y.address ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Address"});
+      data.setState({ Heading: "Sorted By Address" });
     }
     else if (value === "product_title") {
       const sortedData = data.state.article.sort((x, y) => x.product_title < y.product_title ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Product Title"});
+      data.setState({ Heading: "Sorted By Product Title" });
     }
     else if (value === "product_description") {
       const sortedData = data.state.article.sort((x, y) => x.product_description < y.product_description ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Product Description"});
+      data.setState({ Heading: "Sorted By Product Description" });
     }
     else if (value === "date") {
       const sortedData = data.state.article.sort((x, y) => x.date < y.date ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Date"});
+      data.setState({ Heading: "Sorted By Date" });
     }
     else if (value === "status") {
       const sortedData = data.state.article.sort((x, y) => x.status < y.status ? -1 : 1);
       data.setState({ article: sortedData });
-      data.setState({Heading:"Sorted By Status"});
+      data.setState({ Heading: "Sorted By Status" });
     }
   }
-  FilterData(ref,status){
+  FilterData(ref, status) {
     console.log(status)
-      if (status==="All") {
-          ref.setState({article:ref.state.mainarticle});
-          ref.setState({Heading:"All Orders"});
-      }
-      else if (status==="Completed") {
-          const filteredData = ref.state.mainarticle.filter((data)=>{ return data.status===status });
-          ref.setState({article:filteredData})
-          ref.setState({Heading:"Filtered By Status Completed"});
-      }
-      else if (status==="Delivered") {
-          const filteredData = ref.state.mainarticle.filter((data)=>{ return data.status===status });
-          ref.setState({article:filteredData})
-          ref.setState({Heading:"Filtered By Status Delivered"});
-      }
-      else if (status==="Prepared") {
-          const filteredData = ref.state.mainarticle.filter((data)=>{ return data.status===status });
-          ref.setState({article:filteredData})
-          ref.setState({Heading:"Filtered By Status Prepared"});
-      }
-      else if (status==="Prepone") {
-          const filteredData = ref.state.mainarticle.filter((data)=>{ return data.status===status });
-          ref.setState({article:filteredData})
-          ref.setState({Heading:"Filtered By Status Prepone"});
-      }
-      else{
-          const filteredData = ref.state.mainarticle.filter((data)=>{ return data.customer===status });
-          ref.setState({article:filteredData})
-          ref.setState({Heading:"Filtered By Search"});
-      }
+    if (status === "All") {
+      ref.setState({ article: ref.state.mainarticle });
+      ref.setState({ Heading: "All Orders" });
+    }
+    else if (status === "Completed") {
+      const filteredData = ref.state.mainarticle.filter((data) => { return data.status === status });
+      ref.setState({ article: filteredData })
+      ref.setState({ Heading: "Filtered By Status Completed" });
+    }
+    else if (status === "Delivered") {
+      const filteredData = ref.state.mainarticle.filter((data) => { return data.status === status });
+      ref.setState({ article: filteredData })
+      ref.setState({ Heading: "Filtered By Status Delivered" });
+    }
+    else if (status === "Prepared") {
+      const filteredData = ref.state.mainarticle.filter((data) => { return data.status === status });
+      ref.setState({ article: filteredData })
+      ref.setState({ Heading: "Filtered By Status Prepared" });
+    }
+    else if (status === "Prepone") {
+      const filteredData = ref.state.mainarticle.filter((data) => { return data.status === status });
+      ref.setState({ article: filteredData })
+      ref.setState({ Heading: "Filtered By Status Prepone" });
+    }
+    else {
+      const size = status.length;
+      const filteredData = ref.state.mainarticle.filter((data) => { console.log(status,data.customer.substr(0,size)); return data.customer.substr(0,size) === status });
+      ref.setState({ article: filteredData })
+      ref.setState({ Heading: "Filtered By Search" });
+    }
   }
 
   render() {
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -5,38 +5,44 @@ export default class DataTable extends Component {
         var { data, SortingData, outthis } = this.props;
         return (
             <>
-                <div className='TableDataCover'>
-                    <table className='Maintable' cellSpacing="0">
-                        <thead className='MainTablehead'>
-                            <tr>
-                                <th onClick={()=>SortingData(outthis,"order_id")}>ORDER ID <i class="fas fa-sort"></i></th>
-                                <th onClick={()=>SortingData(outthis,"customer")}>CUSTOMER <i class="fas fa-sort"></i></th>
-                                <th onClick={()=>SortingData(outthis,"country")}>COUNTRY <i class="fas fa-sort"></i></th>
-                                <th onClick={()=>SortingData(outthis,"address")}>ADDRESS <i class="fas fa-sort"></i></th>
-                                <th onClick={()=>SortingData(outthis,"product_title")}>PRODUCT_TITLE <i class="fas fa-sort"></i></th>
-                                <th onClick={()=>SortingData(outthis,"product_description")}>DESCRIPTION <i class="fas fa-sort"></i></th>
-                                <th onClick={()=>SortingData(outthis,"date")}>ORDER DATE <i class="fas fa-sort"></i></th>
-                                <th onClick={()=>SortingData(outthis,"status")}>STATUS <i class="fas fa-sort"></i></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((datas) => {
-                                return (
-                                    <tr key={data.order_id}>
-                                        <td className='MainTableOrder'>#{datas.order_id}</td>
-                                        <td>{datas.customer}</td>
-                                        <td>{datas.country}</td>
-                                        <td>{datas.address}</td>
-                                        <td>{datas.product_title}</td>
-                                        <td>{datas.product_description}</td>
-                                        <td>{datas.date}</td>
-                                        <td><div className={`${datas.status}Status`}>{datas.status}</div></td>
+                {data.length === 0 ? <div className='TableNoDataFound'>No Data Found</div> :
+                    <>
+                        < div className='TableDataCover'>
+                            <table className='Maintable' cellSpacing="0">
+                                <thead className='MainTablehead'>
+                                    <tr>
+                                        <th onClick={() => SortingData(outthis, "order_id")}>ORDER ID <i class="fas fa-sort"></i></th>
+                                        <th onClick={() => SortingData(outthis, "customer")}>CUSTOMER <i class="fas fa-sort"></i></th>
+                                        <th onClick={() => SortingData(outthis, "country")}>COUNTRY <i class="fas fa-sort"></i></th>
+                                        <th onClick={() => SortingData(outthis, "address")}>ADDRESS <i class="fas fa-sort"></i></th>
+                                        <th onClick={() => SortingData(outthis, "product_title")}>PRODUCT_TITLE <i class="fas fa-sort"></i></th>
+                                        <th onClick={() => SortingData(outthis, "product_description")}>DESCRIPTION <i class="fas fa-sort"></i></th>
+                                        <th onClick={() => SortingData(outthis, "date")}>ORDER DATE <i class="fas fa-sort"></i></th>
+                                        <th onClick={() => SortingData(outthis, "status")}>STATUS <i class="fas fa-sort"></i></th>
                                     </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+                                </thead>
+                                <tbody>
+                                    {data.map((datas) => {
+                                        return (
+                                            <tr key={data.order_id}>
+                                                <td className='MainTableOrder'>#{datas.order_id}</td>
+                                                <td>{datas.customer}</td>
+                                                <td>{datas.country}</td>
+                                                <td>{datas.address}</td>
+                                                <td>{datas.product_title}</td>
+                                                <td>{datas.product_description}</td>
+                                                <td>{datas.date}</td>
+                                                <td><div className={`${datas.status}Status`}>{datas.status}</div></td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
+
+                }
+
             </>
         );
     }
